4c63eedc189693469767ea403a36bb1a651acc2b
unknown <erfanhosseinizadeh@gmail.com>
2021-10-02 02:04:07 +0330

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>Created By Erfan Git </p>
+				<p>Created By Erfan Git Noob Sag </p>
 				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
 					Learn React
 				</a>
