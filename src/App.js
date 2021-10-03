339d7768503632c0ca769bd61cbb77304e90658e
Erfan <89633581+iamerfan@users.noreply.github.com>
2021-10-03 17:19:45 +0330

    changes

--- src/App.js
+++ src/App.js
@@ -5,29 +5,31 @@ import './App.css';
 function App() {
 	const [city, setcity] = useState('');
 	const [ip, setip] = useState('');
-	const ip_api = 'https://api.geoapify.com/v1/ipinfo?apiKey=f717ab370d274318a46a52c809dab145';
-	fetch(ip_api, {
-		method: 'GET',
-	})
-		.then(function (response) {
-			return response.json();
+
+	//Get IP & City From API
+	function Get_ip() {
+		const Get_ip = 'https://api.geoapify.com/v1/ipinfo?apiKey=f717ab370d274318a46a52c809dab145';
+		fetch(Get_ip, {
+			method: 'GET',
 		})
-		.then(function (resulte) {
-			setcity(resulte.city.name);
-			setip(resulte.ip);
-		});
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (result) {
+				setcity(result.city.name);
+				setip(result.ip);
+			});
+	}
+	Get_ip();
 
 	return (
 		<div className='App'>
-			<header className='App-header'>
+			<main className='App-main'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>Created By Erfan "Git"</p>
-				<p>
-					IP Address = {ip}
-					<br />
-					Location = {city}
-				</p>
-			</header>
+				<p>IP : {ip}</p>
+				<p>Location : {city}</p>
+				<p className='Credit'>Created By Erfan </p>
+			</main>
 		</div>
 	);
 }
