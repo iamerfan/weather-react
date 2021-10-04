583cf48246c88dad9b368a3ee1692b9f2cc073af
Erfan <89633581+iamerfan@users.noreply.github.com>
2021-10-04 21:08:52 +0330

    changes

--- src/App.js
+++ src/App.js
@@ -4,30 +4,41 @@ import './App.css';
 
 function App() {
 	const [city, setcity] = useState('');
+	const [country, setcountry] = useState('');
+	const [flag, setflag] = useState('');
 	const [ip, setip] = useState('');
 
 	//Get IP & City From API
-	function Get_ip() {
-		const Get_ip = 'https://api.geoapify.com/v1/ipinfo?apiKey=f717ab370d274318a46a52c809dab145';
-		fetch(Get_ip, {
-			method: 'GET',
-		})
+	function Get_Data() {
+		fetch('https://api.geoapify.com/v1/ipinfo?apiKey=f717ab370d274318a46a52c809dab145')
 			.then(function (response) {
 				return response.json();
 			})
 			.then(function (result) {
 				setcity(result.city.name);
+				setcountry(result.country.name);
+				setflag('https://www.countryflags.io/' + result.country.iso_code + '/flat/16.png');
 				setip(result.ip);
+				if (city !== '') {
+					document.getElementById('LoadingDiv').style.display = 'none';
+					document.getElementById('DataDiv').style.opacity = '1';
+				}
 			});
 	}
-	Get_ip();
+	Get_Data();
 
 	return (
 		<div className='App'>
 			<main className='App-main'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>IP : {ip}</p>
-				<p>Location : {city}</p>
+				<div id='LoadingDiv' className='LoadingDiv'></div>
+				<div className='DataDiv' id='DataDiv'>
+					<img src={logo} className='App-logo' alt='' />
+					<p>IP : {ip}</p>
+					<p>
+						Location : {city} | {country} <img src={flag} alt='' />
+					</p>
+				</div>
+
 				<p className='Credit'>Created By Erfan </p>
 			</main>
 		</div>
